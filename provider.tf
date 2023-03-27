5f05073912c5e41432c040f764e2c12524e73cea
Clyde Shen <clyde1229@gmail.com>
2023-03-27 15:02:32 +1300

    env variables

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-# setup terraform provider as aws
-# initialize terraform provider as aws
-provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
 terraform {
   required_providers {
     aws = {
@@ -13,3 +5,10 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
--- variables.tf
+++ variables.tf
@@ -1,14 +1,4 @@
 # setup terraform variables for AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY by
-# using terraform workspace environment variables
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
-  description = "AWS Access Key ID"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  description = "Aws Secret Access Key"
-}
 
 variable "AWS_REGION" {
   default     = "ap-southeast-2"
