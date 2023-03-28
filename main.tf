ef16cdddc8dbca6897cb1e482e4baca0707f3797
Clyde Shen <clyde1229@gmail.com>
2023-03-28 16:12:51 +1300

    combine lambda functions to main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+# import module of lambda
+module "lambda" {
+  source = "./modules/lambda"
+}
 
+# module for s3
+module "s3" {
+  source = "./modules/s3"
+}
--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,3 +1,43 @@
+
+# resource for openai lambda function
+resource "aws_lambda_function" "openai" {
+  function_name    = "openai"
+  description      = "Lambda Function for OpenAI"
+  filename         = var.lambda_zip_file_path
+  handler          = "openai.handler"
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = var.LAMBDA_RUNTIME
+  source_code_hash = filebase64sha256(var.lambda_zip_file_path)
+  timeout          = 300
+  memory_size      = 128
+  publish          = true
+  environment {
+    variables = {
+      "S3_BUCKET" = aws_s3_bucket.slz-lambda-functions.id
+    }
+  }
+}
+
+# resource for some lambda function
+resource "aws_lambda_function" "some" {
+  function_name    = "some"
+  description      = "Lambda Function for Some"
+  filename         = var.lambda_zip_file_path
+  handler          = "some.handler"
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = var.LAMBDA_RUNTIME
+  source_code_hash = filebase64sha256(var.lambda_zip_file_path)
+  timeout          = 300
+  memory_size      = 128
+  publish          = true
+  environment {
+    variables = {
+      "S3_BUCKET" = aws_s3_bucket.slz-lambda-functions.id
+    }
+  }
+}
+
+
 # config lambda function policy
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
