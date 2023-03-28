b88f64c20c0c87a86dcc7da7457d245c24f2580b
Clyde Shen <clyde1229@gmail.com>
2023-03-28 15:32:44 +1300

    setup s3, lambda and apigateway

---
+++ modules/lambda/main.tf
@@ -0,0 +1,23 @@
+# config lambda function policy
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+# config lambda function policy attachment
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
